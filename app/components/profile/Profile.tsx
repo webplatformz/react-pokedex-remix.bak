4e2d7c346819ae3b6aa2f936f23f06310b13661a
Felix Mokross <felix.mokross@zuehlke.com>
2023-08-23 18:31:47 +0200

    Add validation

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -1,11 +1,34 @@
 import { Form } from "@remix-run/react";
+import styles from "./Profile.module.css";
 
-export function Profile() {
+type ProfileProps = {
+  values?: ProfileValues;
+  errors?: ProfileErrors;
+};
+
+export type ProfileValues = {
+  name: string;
+  email: string;
+};
+
+export type ProfileErrors = {
+  [K in keyof ProfileValues]?: string;
+};
+
+export function Profile({ values, errors }: ProfileProps) {
   return (
     <Form method="post">
       <div>
         <label htmlFor="name">Name: </label>
-        <input type="text" id="name" name="name" placeholder="Dr. React" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Dr. React"
+          className={errors?.name ? styles.inputError : ""}
+          defaultValue={values?.name}
+        />
+        {errors?.name && <span>{errors.name}</span>}
       </div>
       <div>
         <label htmlFor="email">E-Mail: </label>
@@ -14,7 +37,10 @@ export function Profile() {
           placeholder="react@zuhlke.com"
           id="email"
           name="email"
+          className={errors?.email ? styles.inputError : ""}
+          defaultValue={values?.email}
         />
+        {errors?.email && <span>{errors.email}</span>}
       </div>
       <input type="submit" value="save" />
     </Form>
--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,5 +1,10 @@
 import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+import type {
+  ProfileErrors,
+  ProfileValues,
+} from "~/components/profile/Profile";
 import { Profile } from "~/components/profile/Profile";
 import { commitSession, getSession } from "~/sessions";
 
@@ -10,8 +15,18 @@ export async function action({ request }: ActionArgs) {
   const name = form.get("name");
   const email = form.get("email");
 
-  if (typeof name !== "string") throw new Error("name must be a string");
-  if (typeof email !== "string") throw new Error("email must be a string");
+  if (typeof name !== "string") {
+    throw new Error("name must be string");
+  }
+  if (typeof email !== "string") {
+    throw new Error("email must be string");
+  }
+
+  const errors = validateProfile({ name, email });
+
+  if (hasErrors(errors)) {
+    return json({ values: { name, email }, errors }, { status: 400 });
+  }
 
   session.set("name", name);
   session.set("email", email);
@@ -21,6 +36,29 @@ export async function action({ request }: ActionArgs) {
   });
 }
 
+function validateProfile({ name, email }: ProfileValues) {
+  const errors: ProfileErrors = {};
+
+  if (!name || typeof name !== "string") {
+    errors.name = "Name is required!";
+  } else if (name.length > 25) {
+    errors.name = "The name should be max. 25 characters long!";
+  }
+
+  if (!email || typeof email !== "string") {
+    errors.email = "Email is required!";
+  } else if (!email.match(/^[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i)) {
+    errors.email = "Invalid email!";
+  }
+
+  return errors;
+}
+
+function hasErrors(errors: ProfileErrors) {
+  return Object.values(errors).length > 0;
+}
+
 export default function ProfilePage() {
-  return <Profile />;
+  const actionData = useActionData<typeof action>();
+  return <Profile values={actionData?.values} errors={actionData?.errors} />;
 }
