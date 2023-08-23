7e8eeaa6963b8258e6bbb3f4eddba02baacc30a5
Felix Mokross <felix.mokross@zuehlke.com>
2023-08-23 16:50:13 +0200

    Create simple form and action

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -1,3 +1,31 @@
+import { Form } from "@remix-run/react";
+
 export function Profile() {
-  return <p>To be implemented</p>;
+  return (
+    <Form method="post">
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Dr. React"
+          // className={errors?.name?.type ? styles.inputError : ""}
+        />
+        {/* {errors?.name?.type && <span>{errors.name.message}</span>} */}
+      </div>
+      <div>
+        <label htmlFor="email">E-Mail: </label>
+        <input
+          type="text"
+          placeholder="react@zuhlke.com"
+          id="email"
+          name="email"
+          // className={errors?.name?.type ? styles.inputError : ""}
+        />
+        {/* {errors?.email?.type && <span>{errors.email.message}</span>} */}
+      </div>
+      <input type="submit" value="save" />
+    </Form>
+  );
 }
--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,4 +1,17 @@
+import type { DataFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { Profile } from "~/components/profile/Profile";
+
+export async function action({ request }: DataFunctionArgs) {
+  const form = await request.formData();
+  const name = form.get("name");
+  const email = form.get("email");
+
+  console.log(`name: ${name}`);
+  console.log(`email: ${email}`);
+
+  return redirect(".");
+}
 
 export default function ProfilePage() {
   return <Profile />;
