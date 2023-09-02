04657dc83745eb4ad2989239e3b03df9e6f9ca42
Felix Mokross <felix.mokross@zuehlke.com>
2023-09-02 14:16:06 +0200

    Remove unnecessary loader args on redirect and use explicit LoaderArgs where args are used

--- app/routes/pokemon._index.tsx
+++ app/routes/pokemon._index.tsx
@@ -1,11 +1,10 @@
-import type { DataFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { fetcher } from "~/api/fetcher";
 import type { PokemonResultDto } from "~/api/pokeApi";
 import { PokeList } from "~/components/poke-list/PokeList";
 
-export async function loader(_: DataFunctionArgs) {
+export async function loader() {
   const pokemonList = await fetcher<PokemonResultDto>(
     "https://pokeapi.co/api/v2/pokemon?limit=1000",
   );
